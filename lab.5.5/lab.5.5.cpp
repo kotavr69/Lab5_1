86d99b9c2d476ccb1ce62493cb158737360c17f0
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-11-05 01:16:10 +0700

    Добавьте файлы проекта.

---
+++ lab.5.5/lab.5.5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, i;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	float a[9];
+	cout << "Введите не чётную длину массива n<=9: ";
+	cin >> n;
+	if ((n <= 9) && (n % 2 != 0)) {
+
+		for (i = 0; i < n; i++) {
+			cout << "Введите элемент массива: ";
+			cin >> a[i];
+		}
+		for (i = n-1; i > 0; i--) {
+			if (i % 2 == 0) {
+				continue;
+			}
+			cout << a[i] << endl;
+		}
+	}
+	else {
+		cout << "Неверно заданный элемент массива!";
+	}
+	return 0;
+}
